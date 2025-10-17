89ab36321e4f2c635773cd10682b03f8c40bd730
adexfit <y.adeboye@gmail.com>
2025-10-17 13:21:59 +0100

    edited the readme file

--- README.txt
+++ README.txt
@@ -1,1 +1,9 @@
-## This is the first HNG assignment
+## HNG Profile card 
+- This is project is a profile card built using html and css
+
+## How to run the project 
+- clone the project 
+- open the index.html file using any browser
+
+## hosted link
+[live link](https://codepen.io/realAdexfit/full/yLvbBev)
--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-
-const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const d = new Date();
-const day = weekday[d.getDay()];
-
-document.getElementById("insertDay").innerHTML = day;
-
-
-function updateTime(){
-    const currentTimeV = document.getElementById('insertUTC');
-    const currentTime = new Date();
-    const currentTiMillie = currentTime.getTime();
-    const milliseconds = String(currentTiMillie)
-    currentTimeV.textContent = milliseconds;
-
+function startTime() {
+  offset = 2;
+  let today = new Date();
+  let h = today.getUTCHours();
+  let m = today.getUTCMinutes();
+  let s = today.getUTCSeconds();
+  h = h + offset;
+  if (h > 24) {
+    h = h - 24;
+  }
+  if (h < 0) {
+    h = h + 24;
+  }
+  h = checkTime(h);
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
+  var t = setTimeout(function () {
+    startTime();
+  }, 500);
 }
 
-setInterval(updateTime, 1000);
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
 
-
-updateTime();
+startTime();
